021c3a37aed1f6fb949b9fa7b2213c23e7835f6e
Rajesh Bathula <greeshma-sadineni.com>
2020-05-13 13:13:43 +0200

    first commit

---
+++ Read files Mix and Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Read_files_Mix_and_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\91970\Desktop\Files\text1.txt");
+
+            string[] lines1 = File.ReadAllLines(@"C:\Users\91970\Desktop\Files\text2.txt");
+
+            var mylist = new List<string>();
+            mylist.AddRange(lines);
+            mylist.AddRange(lines1);
+            string[] a = mylist.ToArray();
+            Array.Sort(a);
+
+            foreach (string i in a)
+            {
+                Console.WriteLine(i);
+             
+            }
+         
+            Console.Read();
+        }
+
+    }
+}
